2e1feae38a8511aa6c720a9b823fae9e31453999
mihasok1 <miha3gik@gmail.com>
2021-01-20 18:20:31 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -5,11 +5,30 @@ $(function(){
     starWidth: "12px",
     readOnly: true,
   });
-  var mixer = mixitup('.products__inner-box');
+ 
   $('.product-slider__inner').slick({
     arrows:false,
     slidesToShow:4,
     slidesToScroll: 4,
     dots:true,
   });
+  $("#example_id").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+  });
+  $('.icon-th-list').on('click', function(){
+    $('.products__item').addClass('list');
+    $(this).addClass('active');
+    $('.icon-th-large').removeClass('active');
+  });
+  $('.icon-th-large').on('click', function(){
+    $('.products__item').removeClass('list');
+    $(this).addClass('active');
+    $('.icon-th-list').removeClass('active');
+  });
+  var mixer = mixitup('.products__inner-box');
 });
