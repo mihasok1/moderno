36c5c422909d6ddcb64b81d3c8577495cf3ba489
mihasok1 <miha3gik@gmail.com>
2021-02-01 18:11:08 +0300

    added product-one, 404, about

--- app/js/main.js
+++ app/js/main.js
@@ -36,5 +36,14 @@ $(function(){
   $('.header__btn-menu').on('click', function (){
     $('.header__box').toggleClass('active');  
   });
+  $('.prodect-one__tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.prodect-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+      $('.prodect-one__tabs .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });
+
   var mixer = mixitup('.products__inner-box');
 });
