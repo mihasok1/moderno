ea28bd02f08ff2adb5aa7ffa0a9c3e084acf4a11
mihasok1 <miha3gik@gmail.com>
2021-02-07 12:34:55 +0300

    added contacts

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ gulp.task('css', function () {
     'node_modules/magnific-popup/dist/magnific-popup.css',
     'node_modules/rateyo/src/jquery.rateyo.css',
     'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
   ])
     .pipe(cssmin())
     .pipe(concat('_libs.scss'))
@@ -55,7 +56,8 @@ gulp.task('js', function () {
     'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/rateyo/min/jquery.rateyo.min.js',
-    'node_modules/ion-rangeslider/js/ion.rangeSlider.js'
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
   ])
     .pipe(concat('libs.min.js')) /* объеденяет файлы js в один */
     .pipe(uglify()) /*минифицирует их */
